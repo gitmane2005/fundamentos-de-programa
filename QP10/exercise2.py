cd1df059de479b0380988d07f413891db1a1c15d
Manel Pedro <86663328+gitmane2005@users.noreply.github.com>
2023-12-02 18:09:15 +0000

    Update exercise2.py

--- QP10/exercise2.py
+++ QP10/exercise2.py
@@ -1,5 +1,7 @@
-def comprehensions(i, j):
+"""def comprehensions(i, j):
     t = [x for x in range(i, j+1)if x%10 == 3 or x%10 == 8]
     s = tuple(round(x**(1/2), 2) for x in range(i, j+1))
     dic = {x: chr(x) for x in range(i, j+1)}
-    return (t,s,dic)
+    return (t,s,dic)"""
+
+#o piça
