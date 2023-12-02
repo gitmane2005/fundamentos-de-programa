c3c313df290fadf5e2978ab1a0eaa4e23bac7366
Manel Pedro <86663328+gitmane2005@users.noreply.github.com>
2023-12-02 18:06:55 +0000

    Update exercise2.py

--- QP10/exercise2.py
+++ QP10/exercise2.py
@@ -3,10 +3,3 @@ def comprehensions(i, j):
     s = tuple(round(x**(1/2), 2) for x in range(i, j+1))
     dic = {x: chr(x) for x in range(i, j+1)}
     return (t,s,dic)
-
-
-def union_with(combine, dict1, dict2):
-    all_keys = set(dict1.keys()) | set(dict2.keys())
-    t = {key: combine(dict1.get(key, None), dict2.get(key, None)) for key in all_keys}
-
-    return t
