72a3048b5dff8fe35c88c29b1ac1f4efc130e079
Manel Pedro <86663328+gitmane2005@users.noreply.github.com>
2023-12-02 12:33:45 +0000

    more done

--- QP10/exercise4.py
+++ QP10/exercise4.py
@@ -1,3 +1,11 @@
+"""def union_with(combine, dict1, dict2):
+    #all_keys = set(dict1.keys() | dict2.items())
+    t = {key: combine(value,dict2[key])  if key in dict2.keys() else value for key,value in dict1.items()}#
+    list(map(lambda x: t.update({x[0]: x[1]}) if x[0] not in t.keys() else None, dict2.items()))
+    return t"""
 def union_with(combine, dict1, dict2):
-    return dict2
+    all_keys = set(dict1.keys()) | set(dict2.keys())
+    combined_dict = {key: combine(dict1.get(key), dict2.get(key)) for key in all_keys}
+
+    return combined_dict
 print(sorted(union_with(lambda x,y:x+y, {'a':1, 'b':2}, {'a':3, 'c':4}).items()))
--- te.py
+++ te.py
@@ -1,10 +1,3 @@
-keys = ['a','b','c','d','e']
-values = [1,2,3,4,5]  
- 
-# but this line shows dict comprehension here  
-myDict = { k:v for (k,v) in zip(keys, values)}  
- 
-# We can use below too
-# myDict = dict(zip(keys, values))  
- 
-print (myDict)
+def abs(a,b):
+    return a*b
+print(abs(5,None))
