509c7270dcea5a96e958993d1486b6c1e6dd00be
Manel Pedro <86663328+gitmane2005@users.noreply.github.com>
2024-01-08 16:32:32 +0000

    update

--- playground/Py12: Problem solving/4_ Maximum depth.py
+++ playground/Py12: Problem solving/4_ Maximum depth.py
@@ -1,12 +1,10 @@
 def maximum_depth(l):
-    return max(list(map(lambda x: maximum_depth(x) + 1, l)),1)
-"""def maximum_depth(l):
     if l == []:
         return 1
     else:
         biggest = 0
         for x in l:
             biggest = max(biggest,maximum_depth(x))
-        return (biggest +1)"""
+        return (biggest +1)
 
 print(maximum_depth([[[], [], [[]]], [[]], [], [[]]]))
